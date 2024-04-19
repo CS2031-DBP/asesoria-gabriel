5e6861725bf69b8d4d2689f607151be1c2dedc7b
Gabriel Romero <gabriel.romero@utec.edu.pe>
2024-04-19 01:05:13 -0500

    Avances

---
+++ src/main/java/dbp/e2e/gabriel2/Gabriel2Application.java
@@ -0,0 +1,13 @@
+package dbp.e2e.gabriel2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gabriel2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gabriel2Application.class, args);
+    }
+
+}
---
+++ src/main/java/dbp/e2e/gabriel2/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package dbp.e2e.gabriel2;
+
+import dbp.e2e.gabriel2.author.exception.AuthorNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AuthorNotFoundException.class)
+    public ResponseEntity<String> handleAuthorNotFoundException(AuthorNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+
+
+}
